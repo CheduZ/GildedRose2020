c20ceacbc6dd152e1a3da48fb60309523a6cb583
Veikka Kenttälä <veikka.kenttala@student.oulu.fi>
2024-11-24 14:51:50 +0200

    task 3 done

--- GildedRose/src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ GildedRose/src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -228,5 +228,91 @@ public class GildedRoseTest {
 		assertEquals("Failed quality for Backstage Pass", 0, NewQuality);
 	}
 	
+	@Test
+	public void testBrieQualityAfterSellIn() {
+	    GildedRose inn = new GildedRose();
+	    inn.setItem(new Item("Aged Brie", 0, 10));
+	    List<Item> items = inn.getItems();
+	    int OriginalQuality = items.get(0).getQuality();
+
+	    inn.oneDay();
+	    inn.oneDay();
+
+
+	    int NewQuality = items.get(0).getQuality();
+
+	    assertEquals("Failed quality for Aged Brie after sellIn", OriginalQuality + 4, NewQuality);
+	}
+	@Test
+	public void testBrieQualityMaxAfterSellIn() {
+	    GildedRose inn = new GildedRose();
+	    inn.setItem(new Item("Aged Brie", 0, 49));
+	    List<Item> items = inn.getItems();
+
+	    inn.oneDay();
+	    inn.oneDay();
+
+
+	    int NewQuality = items.get(0).getQuality();
+
+	    assertEquals("Failed quality for Aged Brie after sellIn", 50, NewQuality);
+	}
+	
+	@Test
+	public void testPassQualityMaxAfterSellIn() {
+	    GildedRose inn = new GildedRose();
+	    inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49));
+	    List<Item> items = inn.getItems();
+
+	    inn.oneDay();
+	    inn.oneDay();
+
+
+	    int NewQuality = items.get(0).getQuality();
+
+	    assertEquals("Failed quality for Backstage pass after sellin and going past 50", 0, NewQuality);
+	}
+	
+	@Test
+	public void testNormalItemNegativeQuality() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Elixir of the Mongoose", 5, 0));
+		
+		List<Item> items = inn.getItems();
+		
+		inn.oneDay();	// -1
+			
+		int NewQuality = items.get(0).getQuality();
+		
+		assertEquals("Failed Quality for Elixir (should not be negative)", 0, NewQuality);
+	}
+	@Test
+	public void testNormalItemNegativeQualityAfterSellIn() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Elixir of the Mongoose", 0, 0));
+		
+		List<Item> items = inn.getItems();
+		
+		inn.oneDay();	// -2
+			
+		int NewQuality = items.get(0).getQuality();
+		
+		assertEquals("Failed Quality for Elixir (should not be negative)", 0, NewQuality);
+	}
+	
+	@Test
+	public void testSulfurasQualityAfterSellIn() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+		List<Item> items = inn.getItems();
+		int OriginalQuality = items.get(0).getQuality();
+		
+		inn.oneDay();
+		
+		int NewQuality = items.get(0).getQuality();
+		
+		assertEquals("Failed quality for Sulfuras ", OriginalQuality , NewQuality);
+	}
 	
 }
+	
